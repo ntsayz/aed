f45c69e02622522f61a05c86860f1796f5080b2c
Ntsay Zacarias <ntsayyzacarias@gmail.com>
2022-09-20 16:38:46 +0100

    First commit

---
+++ files/tp01/Tests/funWithCycles.cpp
@@ -0,0 +1,109 @@
+// AED 2022/2023 - Aula Pratica 01
+// Pedro Ribeiro (DCC/FCUP) [ultimo update: 18/09/2022]
+
+#include "funWithCycles.h"
+
+// ----------------------------------------------------------
+// Exercicio 1: Palindromos
+// ----------------------------------------------------------
+// TODO
+bool FunWithCycles::palindrome(const std::string & s) {
+    unsigned length = s.size();
+    for (unsigned i = 0; i < length/2; i++)
+        if (s[i] != s[length-i-1])
+            return false;
+    return true;
+}
+
+// ----------------------------------------------------------
+// Exercicio 2: Frases Palindromo
+// ----------------------------------------------------------
+// TODO
+bool FunWithCycles::palindromeSentence(const std::string & s) {
+    return false;
+}
+
+// ----------------------------------------------------------
+// Exercicio 3: Numeros Interessantes
+// ----------------------------------------------------------
+// TODO
+int FunWithCycles::nextInterestingNumber(int n, int sum) {
+    return 0;
+}
+
+// Esta funcao auxiliar devera devolver a soma dos digitos de n
+// TODO
+int FunWithCycles::digitSum(int n) {
+    return 0;
+}
+
+// ----------------------------------------------------------
+// Exercicio 4: O Inverno esta a chegar
+// ----------------------------------------------------------
+// TODO
+int FunWithCycles::winter(const vector<int> & v) {
+    return 0;
+}
+
+
+// ----------------------------------------------------------
+// Exercicio 5: Brincando com vectores
+// ----------------------------------------------------------
+
+// a) count
+// TODO
+int FunWithCycles::count(const vector<int> & v, int n) {
+    return 0;
+}
+
+// b) hasDuplicates
+// TODO
+bool FunWithCycles::hasDuplicates(const vector<int> & v) {
+    return false;
+}
+
+// c) removeDuplicates
+// TODO
+void FunWithCycles::removeDuplicates(vector<int> & v) {
+
+}
+
+// d) merge
+// TODO
+vector<int> FunWithCycles::merge(const vector<int> & v1, const vector<int> & v2) {
+    vector<int> ans;
+    return ans;
+}
+
+// ----------------------------------------------------------
+// Exercicio 6: Brincando com numeros primos
+// ----------------------------------------------------------
+
+// a) isPrime
+// TODO
+bool FunWithCycles::isPrime(int n) {
+    return false;
+}
+
+// b) factorize
+// TODO
+vector<int> FunWithCycles::factorize(int n) {
+    vector<int> ans;
+    return ans;
+}
+
+// c) listPrimes
+// TODO
+vector<int> FunWithCycles::listPrimes(int n) {
+    vector<int> ans;
+    return ans;
+}
+
+
+// ----------------------------------------------------------
+// Exercicio 7: Numeros interessantes (versao desafio)
+// ----------------------------------------------------------
+// TODO
+long long FunWithCycles::fastNextInterestingNumber(long long n, int sum) {
+    return 0;
+}
