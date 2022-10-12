d65aaa7af1a34e4d08843e625f2e8f44ce799725
Ntsay Zacarias <ntsayyzacarias@gmail.com>
2022-10-12 17:18:10 +0100

    tp04

---
+++ cmake-build-debug/CMakeFiles/aed.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aed.dir/main.cpp.o"
+  "CMakeFiles/aed.dir/main.cpp.o.d"
+  "aed"
+  "aed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
